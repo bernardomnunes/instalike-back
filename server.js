fcf3dc455e9868d01c96a419c2b811f452a47638
bernardo nunes <bmn02@outlook.com>
2024-11-19 19:30:21 -0300

    feat: add posts and post routes

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
 import express from "express";
 
+const posts = [
+  {
+    id: 1,
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    image: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 2,
+    description: "Un altro post interessante con dettagli aggiuntivi.",
+    image: "https://picsum.photos/id/237/300/200",
+    category: "tecnologia",
+  },
+];
+
+console.log(posts[1]);
+
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .send(
-      "lorim ipsum dolor sit amet consectetur adipisicing elit fjdsalkjflkdsa jfkl dsajlk fjdsalk jfdsakl; jfkldsajfkldasjklfdjsalfjdsalkjf"
-    );
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts);
+});
+
+function searchPost(id) {
+  return posts.findIndex((post) => post.id === Number(id));
+}
+
+app.get("/posts/:id", (req, res) => {
+  const index = searchPost(req.params.id);
+  console.log(index);
+  res.status(200).json(posts[index]);
 });
