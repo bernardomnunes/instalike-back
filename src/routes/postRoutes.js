49dc006f135a2b32c55a9c6abeb2503595083600
bernardo nunes <bmn02@outlook.com>
2024-11-21 13:57:47 -0300

    feat: add modularization

---
+++ src/routes/postRoutes.js
@@ -0,0 +1,11 @@
+// rotas do servidor
+
+import express from "express";
+import { listarPosts } from "../controllers/postsController.js";
+
+const routes = (app) => {
+  app.use(express.json());
+  app.get("/posts", listarPosts);
+};
+
+export default routes;
